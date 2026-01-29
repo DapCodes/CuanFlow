a7391eaa0e60ae94d813805dcf357b30675915bd
DapCodes <daffaramadhan929@gmail.com>
2026-01-29 13:18:31 +0700

    add: menambahkan fitur icon untuk payment

--- database/seeders/PaymentMethodSeeder.php
+++ database/seeders/PaymentMethodSeeder.php
@@ -10,22 +10,22 @@ class PaymentMethodSeeder extends Seeder
     public function run(): void
     {
         $methods = [
-            ['name' => 'Bank BCA', 'code' => 'bca', 'icon' => null],
-            ['name' => 'Bank BRI', 'code' => 'bri', 'icon' => null],
-            ['name' => 'Bank BNI', 'code' => 'bni', 'icon' => null],
-            ['name' => 'Bank Mandiri', 'code' => 'mandiri', 'icon' => null],
-            ['name' => 'Bank Permata', 'code' => 'permata', 'icon' => null],
-            ['name' => 'Bank CIMB Niaga', 'code' => 'cimb', 'icon' => null],
-            ['name' => 'QRIS', 'code' => 'qris', 'icon' => null],
-            ['name' => 'GoPay', 'code' => 'gopay', 'icon' => null],
-            ['name' => 'OVO', 'code' => 'ovo', 'icon' => null],
-            ['name' => 'DANA', 'code' => 'dana', 'icon' => null],
-            ['name' => 'ShopeePay', 'code' => 'shopeepay', 'icon' => null],
-            ['name' => 'LinkAja', 'code' => 'linkaja', 'icon' => null],
+            ['name' => 'Bank BCA', 'code' => 'bca', 'icon' => 'assets/bank/bca.png'],
+            ['name' => 'Bank BRI', 'code' => 'bri', 'icon' => 'assets/bank/bri.png'],
+            ['name' => 'Bank BNI', 'code' => 'bni', 'icon' => 'assets/bank/bni.jpg'],
+            ['name' => 'Bank Mandiri', 'code' => 'mandiri', 'icon' => 'assets/bank/mandiri.png'],
+            ['name' => 'Bank Permata', 'code' => 'permata', 'icon' => 'assets/bank/permata-bank.jpg'],
+            ['name' => 'Bank CIMB Niaga', 'code' => 'cimb', 'icon' => 'assets/bank/cimb-niaga.png'],
+            ['name' => 'QRIS', 'code' => 'qris', 'icon' => 'assets/bank/qris.png'],
+            ['name' => 'GoPay', 'code' => 'gopay', 'icon' => 'assets/bank/gopay.png'],
+            ['name' => 'OVO', 'code' => 'ovo', 'icon' => 'assets/bank/ovo.jpg'],
+            ['name' => 'DANA', 'code' => 'dana', 'icon' => 'assets/bank/dana.png'],
+            ['name' => 'ShopeePay', 'code' => 'shopeepay', 'icon' => 'assets/bank/shopeepay.png'],
+            ['name' => 'LinkAja', 'code' => 'linkaja', 'icon' => 'assets/bank/link-aja.png'],
         ];
 
         foreach ($methods as $method) {
-            PaymentMethod::firstOrCreate(
+            PaymentMethod::updateOrCreate(
                 ['code' => $method['code']],
                 array_merge($method, ['is_active' => true])
             );
