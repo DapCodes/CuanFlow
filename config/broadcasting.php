54d9836daf7479e7d200bf3e72ff578318404278
DapCodes <daffaramadhan929@gmail.com>
2026-04-08 14:54:22 +0700

    fix: disable SSL verification for Pusher curl requests in broadcasting config

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -11,13 +11,18 @@ return [
             'key' => env('PUSHER_APP_KEY'),
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
+
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER', 'ap1'),
                 'useTLS' => true,
+
                 'curl_options' => [
                     CURLOPT_IPRESOLVE => CURL_IPRESOLVE_V4,
                     CURLOPT_CONNECTTIMEOUT => 30,
                     CURLOPT_TIMEOUT => 30,
+                    
+                    CURLOPT_SSL_VERIFYPEER => false,
+                    CURLOPT_SSL_VERIFYHOST => 0,
                 ],
             ],
         ],
